06b5d662c812888e2b88c5c141e009379d72bb8c
arty_d <artemzagorulko@hotmail.com>
2020-12-12 20:07:06 +0200

    first commit

---
+++ couponservise/src/main/java/com/bharath/springcloud/CouponserviseApplication.java
@@ -0,0 +1,13 @@
+package com.bharath.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponserviseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponserviseApplication.class, args);
+	}
+
+}
